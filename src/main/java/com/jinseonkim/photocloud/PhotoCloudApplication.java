0c902386eb107997bcc9c9943aa3e086d08a7d58
김진선 <jinseon@pulit.jp>
2020-02-07 17:13:52 +0900

    first commit

---
+++ src/main/java/com/jinseonkim/photocloud/PhotoCloudApplication.java
@@ -0,0 +1,13 @@
+package com.jinseonkim.photocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jinseonkim/photocloud/PhotoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinseonkim.photocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
