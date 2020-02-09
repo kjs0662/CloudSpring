604aaa93ebae836340617b94ee7b8098a01beba0
jinseonkim <k_dooly@naver.com>
2020-02-10 01:04:30 +0900

    second

--- src/main/java/com/jinseonkim/photocloud/PhotoCloudApplication.java
+++ src/main/java/com/jinseonkim/photocloud/PhotoCloudApplication.java
@@ -1,13 +1,26 @@
 package com.jinseonkim.photocloud;
 
+import com.jinseonkim.photocloud.storage.StorageProperties;
+import com.jinseonkim.photocloud.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class PhotoCloudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PhotoCloudApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.init();
+		};
 	}
 
 }
